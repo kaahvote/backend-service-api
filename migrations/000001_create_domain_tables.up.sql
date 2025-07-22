6ec1338622b2968351a6ef2185a3e5b39a30825c
George Nunes <georgebnunes@gmail.com>
2025-07-21 23:41:52 -0300

    chore: migrations for domain tables

--- migrations/000001_create_domain_tables.up.sql
+++ migrations/000001_create_domain_tables.up.sql
@@ -1,25 +1,25 @@
 CREATE TABLE "voting_policies" (
   "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "name" varchar NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL
+  "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE "voter_policies" (
   "id" integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "name" varchar NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL
+  "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE "candidate_policies" (
   "id" integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "name" varchar NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL
+  "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE "session_states" (
   "id" integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "name" varchar NOT NULL,
-  "created_at" TIMESTAMPTZ NOT NULL
+  "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE "users" (
@@ -28,5 +28,5 @@ CREATE TABLE "users" (
   "name" varchar,
   "email" varchar,
   "password" varchar,
-  "created_at" TIMESTAMPTZ NOT NULL
+  "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
