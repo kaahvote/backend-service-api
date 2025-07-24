8c1b0c30b90125ca966a3a76795c68671fead49c
George Nunes <georgebnunes@gmail.com>
2025-07-23 23:57:52 -0300

    feat: fetch single session by public_id

--- internal/data/sessions.go
+++ internal/data/sessions.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -24,7 +25,42 @@ type SessionModel struct {
 	DB *sql.DB
 }
 
-func (m *SessionModel) Insert(s *Session) error {
+func (m SessionModel) Get(publicId string) (*Session, error) {
+	query := `
+		SELECT id, name, public_id, expires_at, voting_policy_id, 
+		voters_policy_id, candidate_policy_id, created_by, created_at
+		FROM sessions WHERE public_id=$1`
+
+	var s Session
+
+	ctx, cancel := context.WithTimeout(context.Background(), THREE_SECONDS)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, publicId).Scan(
+		&s.ID,
+		&s.Name,
+		&s.PublicID,
+		&s.ExpiresAt,
+		&s.VotingPolicyID,
+		&s.VotersPolicyID,
+		&s.CandidatesPolicyID,
+		&s.CreatedBy,
+		&s.CreatedAt,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &s, nil
+}
+
+func (m SessionModel) Insert(s *Session) error {
 	query := `INSERT INTO sessions (name, public_id, expires_at, voting_policy_id, voters_policy_id, candidate_policy_id, created_by) VALUES
 				($1, $2, $3, $4, $5, $6, $7) 
 				RETURNING id, created_at`
