1fa8e94ea1ebac586b5d9d3a9595415274aa2584
George Nunes <georgebnunes@gmail.com>
2025-07-30 23:34:48 -0300

    feat: ACID insert of sessions and flows

--- cmd/api/sessions.go
+++ cmd/api/sessions.go
@@ -90,12 +90,6 @@ func (app *application) postSessionHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	err = app.models.Flows.InsertFirstFlow(session.ID)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
-
 	headers := make(http.Header)
 	headers.Set("Location", fmt.Sprintf("/v1/sessions/%s", session.PublicID))
 
--- internal/data/constants.go
+++ internal/data/constants.go
@@ -3,3 +3,4 @@ package data
 import "time"
 
 const THREE_SECONDS = 3 * time.Second
+const SESSION_IN_DRAFT = 1
