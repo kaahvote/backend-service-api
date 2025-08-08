95a0cf983be093c91283e34eea8075ad5a83b543
George Nunes <georgebnunes@gmail.com>
2025-08-08 12:49:29 -0300

    feat(sessions): update when advance voting status

--- internal/data/flows.go
+++ internal/data/flows.go
@@ -17,13 +17,6 @@ type Flow struct {
 	CreatedAt       time.Time `json:"createdAt"`
 }
 
-type FlowDetail struct {
-	ID          int64     `json:"id"`
-	Comment     string    `json:"comment,omitzero"`
-	CreatedAt   time.Time `json:"createdAt"`
-	StateDetail State     `json:"stateDetail"`
-}
-
 func (f Flow) Equals(flow *Flow) bool {
 	return f.SessionID == flow.SessionID &&
 		f.StateID == flow.StateID
@@ -87,34 +80,6 @@ func (m FlowModel) GetCurrentState(sessionID int64) (*Flow, error) {
 	}
 
 	return &f, nil
-}
-
-func (m FlowModel) GetCurrentFlow(sessionID int64) (*FlowDetail, error) {
-	query := `SELECT
-	  			f.id, s.id, s.name, f.comment, f.created_at
-			  FROM flows f
-			  INNER JOIN session_states s ON s.id = f.state_id
-			  WHERE f.session_id = $1
-			  ORDER BY created_at DESC
-			  LIMIT 1`
-
-	ctx, cancel := context.WithTimeout(context.Background(), THREE_SECONDS)
-	defer cancel()
-
-	var f FlowDetail
-	var comment sql.NullString
-	err := m.DB.QueryRowContext(ctx, query, sessionID).Scan(&f.ID, &f.StateDetail.ID, &f.StateDetail.Name, &comment, &f.CreatedAt)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if comment.Valid {
-		f.Comment = comment.String
-	}
-
-	return &f, nil
-
 }
 
 func (m FlowModel) GetFullHistory(filters FlowFilters) ([]*Flow, Metadata, error) {
